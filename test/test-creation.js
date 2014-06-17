a6415e96c5eda3c675c09603bf443eb870c90582
Cédric <cedced19@gmail.com>
2014-06-17 11:39:19 +0200

    Update test-creation.js

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,36 +1,75 @@
-/*global describe, beforeEach, it */
+//Todo
 'use strict';
+var util = require('util');
 var path = require('path');
-var helpers = require('yeoman-generator').test;
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
 
-describe('JadeStyl generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
+
+var JadestylGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
       }
+    });
+  },
 
-      this.app = helpers.createGenerator('JadeStyl:app', [
-        '../../app'
-      ]);
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the marvelous Jadestyl generator!'));
+
+    var prompts = [{
+      name: 'title',
+      message: 'What is the title of your application?',
+      default: 'Hello World'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.title = props.title;
+
       done();
+
+  var extractGeneratorName = function (_, appname) {
+    var slugged = _.slugify(title);
+    var match = slugged.match(/^$/);
+
+    if (match && match.length === 2) {
+      return match[1].toLowerCase();
+  }
+
+  return slugged;
+  };
     }.bind(this));
-  });
+  },
 
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
-    ];
 
-    helpers.mockPrompt(this.app, {
-      'someOption': true
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
-    });
-  });
+  app: function () {
+    this.mkdir('src');
+    this.mkdir('src/scripts');
+    this.mkdir('src/styles');
+
+    this.template('src/index.jade', 'src/index.jade');
+    this.template('src/scripts/main.js', 'src/scripts/main.js');
+    this.template('src/styles/main.styl', 'src/styles/main.styl');
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('gitignore', '.gitignore');
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('jshintrc', '.jshintrc');
+    this.template('Gruntfile.js', 'Gruntfile.js');
+    this.template('README.md', 'README.md');
+  },
+
+
+  projectfiles: function () {
+  }
 });
+
+module.exports = JadestylGenerator;
